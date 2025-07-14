cbb9696539ae4cb18ee80022a57b8cf95afa5a0a
Taj Eddine Marmoul <tajdinetajdine1@gmail.com>
2025-07-14 08:11:18 +0100

    updated excel manager to include sort by Tec and combine datetime

--- dashboard/excel_manager.py
+++ dashboard/excel_manager.py
@@ -3,26 +3,27 @@ from typing import Optional
 import polars as pl
 
 
-from config import get_config_dir,config,load_config,save_config
+from config import get_config_dir, config, load_config, save_config
 
-from os.path import  exists
+from os.path import exists
 
-# init 
+# init
 
-name_config = config.get("config",{}).get("config_data_name","config_data.toml")
+name_config = config.get("config", {}).get("config_data_name", "config_data.toml")
 
 dir_main_config = get_config_dir()
 
-path_config = os.path.join(dir_main_config,name_config)
+path_config = os.path.join(dir_main_config, name_config)
 
 
-config  = load_config(path_config)
+config = load_config(path_config)
 
 
-path_to_excel = config.get("path_to_excel","")
+path_to_excel = config.get("path_to_excel", "")
 
 
 # func
+
 
 def path_exits():
     global path_to_excel
@@ -30,41 +31,65 @@ def path_exits():
     return bool(path_to_excel)
 
 
+def filter_tec(df_lazy: pl.LazyFrame) -> pl.LazyFrame:
 
-def update_path_to_excel(path)-> tuple[bool,str]:
-    global config,path_to_excel
-    
+    return df_lazy.filter(pl.col("LIB_CODE_DR") == "TEC")
+
+
+def create_dep_datetime(df_lazy: pl.LazyFrame) -> pl.LazyFrame:
+
+    return df_lazy.with_columns(
+        [
+            (pl.col("DEP_DAY_SCHED").cast(pl.Utf8) + " " + pl.col("DEP_TIME_SCHED"))
+            .str.strptime(pl.Datetime, "%Y-%m-%d %H:%M", strict=False)
+            .alias("DEP_DATETIME")
+        ]
+    )
+
+
+lf_dt = create_dep_datetime("PATH", "SHEET")
+df_dt = lf_dt.collect()
+
+
+def update_path_to_excel(path) -> tuple[bool, str]:
+    global config, path_to_excel
+
     is_exist = exists(path)
-    
+
     if is_exist:
         config["path_to_excel"] = path
         path_to_excel = path
         try:
             load_excel_lazy(path)
         except:
-            return False,"The excel file is corrupted ."
-        save_config(path_config,config)
-        return True,"Loaded The File successfuly ."
-    return is_exist,"The excel file doesn't exists ."
-        
-def load_excel_lazy(path)-> Optional[pl.LazyFrame]:
+            return False, "The excel file is corrupted ."
+        save_config(path_config, config)
+        return True, "Loaded The File successfuly ."
+    return is_exist, "The excel file doesn't exists ."
+
+
+def load_excel_lazy(path) -> Optional[pl.LazyFrame]:
     if not path:
         return None
-    
+
     is_exist = exists(path)
-    
+
     if not is_exist:
         return None
 
-    return pl.read_excel(path,sheet_name="Sheet1").lazy()
-    
+    res = pl.read_excel(path, sheet_name="Sheet1").lazy()
+
+    res = filter_tec(res)
+    res = create_dep_datetime(res)
+
+    return res
 
 
 def get_df() -> Optional[pl.LazyFrame]:
     return df
 
 
-# program 
+# program
 
 
 df = load_excel_lazy(path_to_excel)
--- dashboard/root.py
+++ dashboard/root.py
@@ -6,56 +6,66 @@ import dash_bootstrap_components as dbc
 
 from dashboard.server_instance import get_app
 
-from excel_manager import get_df,path_exits
+from excel_manager import get_df, path_exits
 
 app = get_app()
-
-
-
-
 
 
 # Données pour les éléments de navigation
 if path_exits():
     import dashboard.pages.home.page as home
     import dashboard.pages.tech.page as tech
+
     nav_items = [
         {"name": "Dashboard", "icon": "fas fa-home", "href": "/", "page": home.layout},
-        {"name": "Analytics", "icon": "fas fa-chart-line", "href": "/analytics", "page": tech.layout},
-        {"name": "Settings", "icon": "fas fa-cog", "href": "/settings", "page": html.Div("Paramètres")},
+        {
+            "name": "Analytics",
+            "icon": "fas fa-chart-line",
+            "href": "/analytics",
+            "page": tech.layout,
+        },
+        {
+            "name": "Settings",
+            "icon": "fas fa-cog",
+            "href": "/settings",
+            "page": html.Div("Paramètres"),
+        },
     ]
 else:
     import dashboard.pages.verify.page as verify
 
     nav_items = [
-        {"name": "verifye", "icon": "fas fa-home", "href": "/", "page": verify.layout,"show":False}
+        {
+            "name": "verify",
+            "icon": "fas fa-home",
+            "href": "/",
+            "page": verify.layout,
+            "show": False,
+        }
     ]
 
 navbar = dbc.Nav(
     children=[
-        
-            dbc.NavItem(dbc.NavLink(nav_item["name"], href=nav_item["href"]))
-        for nav_item in nav_items if nav_item.get("show",True)
-    ],    
+        dbc.NavItem(dbc.NavLink(nav_item["name"], href=nav_item["href"]))
+        for nav_item in nav_items
+        if nav_item.get("show", True)
+    ],
     className="justify-content-center nav-tabs",
-    id="navbar",   
+    id="navbar",
 )
 
 
+app.layout = html.Div(
+    [
+        # Barre de navigation
+        navbar,
+        # Contenu principal
+        html.Div(id="page-content"),
+        # Stockage pour suivre l'état du menu
+        dcc.Location(id="url"),
+    ]
+)
 
-
-
-app.layout = html.Div([
-    # Barre de navigation
-    navbar,
-    
-    # Contenu principal
-    html.Div(id="page-content"),
-    
-    
-    # Stockage pour suivre l'état du menu
-    dcc.Location(id='url')
-    ])
 
 @app.callback(Output("navbar", "children"), Input("url", "pathname"))
 def update_navbar(pathname):
@@ -65,19 +75,20 @@ def update_navbar(pathname):
             dbc.NavLink(
                 nav_item["name"],
                 href=nav_item["href"],
-                active=pathname == nav_item["href"]  
+                active=pathname == nav_item["href"],
             )
         )
-        for nav_item in nav_items if nav_item.get("show",True)
+        for nav_item in nav_items
+        if nav_item.get("show", True)
     ]
 
 
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page(pathname):
     for nav_item in nav_items:
-        if pathname == nav_item["href"] :
+        if pathname == nav_item["href"]:
             return nav_item["page"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
