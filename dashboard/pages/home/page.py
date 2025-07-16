17832ca925c87729484effe56a455853c4b7c4d8
Taj Eddine Marmoul <tajdinetajdine1@gmail.com>
2025-07-16 10:53:04 +0100

    main page

--- dashboard/pages/home/page.py
+++ dashboard/pages/home/page.py
@@ -253,7 +253,7 @@ def search_flight(n_clicks, ac_type, ac_reg, dt_start_str, dt_end_str):
             "CODE_DR",
         ]
 
-        if df is None or df.execute().is_empty():
+        if df is None or df.is_empty():
             return (
                 "",
                 False,
